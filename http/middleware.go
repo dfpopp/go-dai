b740378d6ee477877b04cfa7d58169aecef6d703
戴方平 <5176369+daifangping@user.noreply.gitee.com>
2025-12-03 10:16:56 +0800

    初始化

--- http/middleware.go
+++ http/middleware.go
@@ -1,8 +1,6 @@
 package http
 
 import (
-	"fmt"
-	"github.com/dfpopp/go-dai/db/redisDb"
 	"net/http"
 )
 
@@ -14,17 +12,8 @@ type HandlerFunc func(*Context)
 
 // Recovery 异常恢复中间件
 func Recovery() MiddlewareFunc {
-	fmt.Println("hello")
-	defer func() {
-		err := redisDb.CloseRedis()
-		if err != nil {
-			fmt.Println("《《关闭错误：" + err.Error())
-		}
-	}()
 	return func(next HandlerFunc, c *Context) {
-		fmt.Println("hello1")
 		defer func() {
-			fmt.Println("hello2")
 			if err := recover(); err != nil {
 				c.JSON(http.StatusInternalServerError, map[string]interface{}{
 					"code": 500,
