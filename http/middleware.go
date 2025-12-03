d8ef424fa23172b641c03f1a6170bcdcd4daf634
戴方平 <5176369+daifangping@user.noreply.gitee.com>
2025-12-03 10:13:45 +0800

    初始化

--- http/middleware.go
+++ http/middleware.go
@@ -14,6 +14,7 @@ type HandlerFunc func(*Context)
 
 // Recovery 异常恢复中间件
 func Recovery() MiddlewareFunc {
+	fmt.Println("hello")
 	defer func() {
 		err := redisDb.CloseRedis()
 		if err != nil {
@@ -21,7 +22,9 @@ func Recovery() MiddlewareFunc {
 		}
 	}()
 	return func(next HandlerFunc, c *Context) {
+		fmt.Println("hello1")
 		defer func() {
+			fmt.Println("hello2")
 			if err := recover(); err != nil {
 				c.JSON(http.StatusInternalServerError, map[string]interface{}{
 					"code": 500,
